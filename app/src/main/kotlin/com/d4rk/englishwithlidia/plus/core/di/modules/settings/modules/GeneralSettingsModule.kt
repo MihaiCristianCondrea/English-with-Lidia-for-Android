5de900ff98f6ccb98e4c2d112e3a7115ee131188
D4rK7355608 <d4rk7355608@gmail.com>
2026-01-28 09:23:47 +0200

    Refactor changelog to use App Toolkit component

--- app/src/main/kotlin/com/d4rk/englishwithlidia/plus/core/di/modules/settings/modules/GeneralSettingsModule.kt
+++ app/src/main/kotlin/com/d4rk/englishwithlidia/plus/core/di/modules/settings/modules/GeneralSettingsModule.kt
@@ -3,8 +3,8 @@ package com.d4rk.englishwithlidia.plus.core.di.modules.settings.modules
 import com.d4rk.android.libs.apptoolkit.app.settings.general.data.repository.GeneralSettingsRepositoryImpl
 import com.d4rk.android.libs.apptoolkit.app.settings.general.domain.repository.GeneralSettingsRepository
 import com.d4rk.android.libs.apptoolkit.app.settings.general.ui.GeneralSettingsViewModel
-import com.d4rk.android.libs.apptoolkit.app.settings.general.ui.content.GeneralSettingsContentProvider
 import com.d4rk.android.libs.apptoolkit.app.settings.utils.providers.DisplaySettingsProvider
+import com.d4rk.android.libs.apptoolkit.app.settings.utils.providers.GeneralSettingsContentProvider
 import com.d4rk.android.libs.apptoolkit.app.settings.utils.providers.PrivacySettingsProvider
 import com.d4rk.englishwithlidia.plus.app.settings.settings.utils.providers.AppDisplaySettingsProvider
 import com.d4rk.englishwithlidia.plus.app.settings.settings.utils.providers.AppPrivacySettingsProvider
